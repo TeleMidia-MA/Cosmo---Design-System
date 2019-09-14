a58c4f0454cc60ef779a04a71b6ef17c636470f9
Micael <micaellgoms@gmail.com>
2019-09-14 19:51:58 -0300

    feat(bootstrap_4): build using sass-loader
    - Native Bootstrap v4
    - Grunt to SASS

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+
+import "./assets/scss/style.scss"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <img src={logo}/>
+
+        <div class="alert alert-primary" role="alert">
+          Um simples alerta primary. Olha só!
+        </div>
+
       </header>
     </div>
   );
